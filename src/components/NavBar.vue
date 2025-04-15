cdbb35f52d1780133367b246c6e0f96dccd70ae1
Ratkov_Daniil <daniilratkov3@gmail.com>
2025-04-15 22:05:44 +0700

    init commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NavButton from './NavButton.vue'
+</script>
+
+<template>
+  <div class="nav-bar">
+    <img
+      src="https://i.pinimg.com/736x/94/e2/7b/94e27b9af7ab8c18e9af0338e2362228.jpg"
+      alt=""
+      class="nav-bar__icon"
+    />
+    <div class="nav-bar__inner">
+      <NavButton :link="`/`">Home</NavButton>
+      <NavButton :link="`/`">Services</NavButton>
+      <NavButton :link="`/`">Feature</NavButton>
+      <NavButton :link="`/`">Product</NavButton>
+      <NavButton :link="`/`">FAQ</NavButton>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/NavButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  link: String,
+  className: String,
+})
+</script>
+
+<template>
+  <RouterLink :to="props.link">
+    <button :class="`${props.className}`"><slot /></button>
+  </RouterLink>
+</template>
